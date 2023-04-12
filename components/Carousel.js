fad4fbad1243c7ed25d2880ad6e378e7f3f00092
JTris1 <johnathontrisler@gmail.com>
2023-04-12 19:38:10 -0400

    Improved carousel on home

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper';
+import { Navigation, Autoplay } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import CarouselItem from './CarouselItem';
@@ -10,19 +10,35 @@ function Carousel({ slides }) {
 
     return (
         <>
-            <div className='bg-[#c4c4c4] w-full h-96 mt-32 rounded-2xl flex justify-center'>
+            <div className='bg-white w-9/12 h-96 mt-32 rounded-2xl flex justify-center'>
                 <Swiper
-                    modules={[Navigation]}
+                    modules={[Navigation, Autoplay]}
                     slidesPerView={1}
                     navigation
                     onSlideChange={() => console.log('slide change')}
                     centeredSlides={true}
+                    loop={true}
+                    autoplay={{
+                        delay: 5000,
+                        pauseOnMouseEnter: true,
+                        reverseDirection: true
+                    }}
+
+
+                    style={{
+                        '--swiper-navigation-size': '32px',
+                        '--swiper-navigation-sides-offset': '100px',
+                        '--swiper-navigation-color': '#1282A2'
+                    }}
                 >
                     <SwiperSlide style={{ display: 'flex' }} className='justify-center items-center'>
-                        <CarouselItem price={250000} city={'Indianapolis'} state={'IN'} bedrooms={3} baths={2} />
+                        <CarouselItem imgSrc={'/example-house.jpg'} price={250000} city={'Indianapolis'} state={'IN'} bedrooms={3} baths={2} />
                     </SwiperSlide>
                     <SwiperSlide style={{ display: 'flex' }} className='justify-center items-center'>
-                        <CarouselItem />
+                        <CarouselItem imgSrc={'/example-house.jpg'} price={250000} city={'Indianapolis'} state={'IN'} bedrooms={3} baths={2} />
+                    </SwiperSlide>
+                    <SwiperSlide style={{ display: 'flex' }} className='justify-center items-center'>
+                        <CarouselItem imgSrc={'/example-house.jpg'} price={250000} city={'Indianapolis'} state={'IN'} bedrooms={3} baths={2} />
                     </SwiperSlide>
                 </Swiper>
             </div>
--- components/CarouselItem.js
+++ components/CarouselItem.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
 function CarouselItem({ imgSrc, price, city, state, bedrooms, baths }) {
     let USD = new Intl.NumberFormat('en-US', {
@@ -8,32 +9,27 @@ function CarouselItem({ imgSrc, price, city, state, bedrooms, baths }) {
     });
 
     return (
-        <div className='flex w-2/3 h-4/5'>
-            <div className='w-5/12 h-full bg-[#000] rounded-s-xl'>
-
+        <div className='flex w-2/3 h-4/5 drop-shadow-xl'>
+            <div className='w-5/12 h-full bg-white rounded-s-xl relative'>
+                <Image src={imgSrc} alt="House" fill={true} className='object-cover rounded-s-xl' />
             </div>
-            <div className='w-7/12 h-full flex flex-col justify-between items-center bg-blue-700 rounded-e-xl p-8'>
+            <div className='w-7/12 h-full flex flex-col justify-between items-center bg-blue-500 rounded-e-xl p-8'>
                 <div className='text-center text-white'>
-                    <h2 className='text-4xl'>{USD.format(price)}</h2>
-                    <h4>{city}, {state}</h4>
+                    <h2 className='text-4xl font-bold'>{USD.format(price)}</h2>
+                    <h4 className='text-xl'>{city}, {state}</h4>
                 </div>
                 <div className='text-center text-white flex justify-evenly w-full'>
-                    <span>{bedrooms}</span>
-                    <span>{baths}</span>
+                    <span className='inline-flex items-center'>
+                        <span className="material-symbols-outlined text-5xl mx-5">bed</span>
+                        <p className='text-3xl font-bold'>{bedrooms}</p>
+                    </span>
+                    <span className='inline-flex items-center'>
+                        <span className="material-symbols-outlined text-5xl mx-5">bathtub</span>
+                        <p className='text-3xl font-bold'>{baths}</p>
+                    </span>
                 </div>
             </div>
         </div>
-        // <div className='w-2/3 h-4/5 bg-blue-700 flex flex-col rounded-xl'>
-        //     <div className='w-5/12 h-full bg-[#000] rounded-s-xl'>
-        //         <div>
-        //             <h2>{price}</h2>
-        //             <h4>{city}, {state}</h4>
-        //         </div>
-        //         <div>
-
-        //         </div>
-        //     </div>
-        // </div>
     )
 }
 
