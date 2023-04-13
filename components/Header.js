350cd57953a6ac8e00be733d3cf3ae68878fd0ba
JTris1 <johnathontrisler@gmail.com>
2023-04-13 14:49:43 -0400

    Widened hitbox for account dropdown

--- components/Header.js
+++ components/Header.js
@@ -12,10 +12,10 @@ function Header() {
             <header className={`flex justify-center items-center px-${header_x_pad_marg} py-5 bg-blue-300 box-border relative`} id='navbar'>
                 {/* <span className="material-symbols-outlined invisible">account_circle</span> */}
                 <Image src="/logo-light.png" height={64} width={64} alt="Logo" className='h-auto' />
-                <div className={`absolute right-0 h-full mr-${header_x_pad_marg} flex items-center`}
+                <div className={`absolute right-0 h-full w-16 mr-${header_x_pad_marg} flex items-center justify-center cursor-pointer`}
                     onMouseOver={() => setAccPaneOpen(true)}
                     onMouseLeave={() => setAccPaneOpen(false)}>
-                    <span className="material-symbols-outlined text-5xl text-white cursor-pointer">account_circle</span>
+                    <span className="material-symbols-outlined text-5xl text-white">account_circle</span>
                 </div>
             </header>
             {accPaneOpen ? <AccountPane setAccPaneOpen={setAccPaneOpen} /> : undefined}
