da7865258a49ea072589aef6cf27b6169c4aaa1f
JTris1 <johnathontrisler@gmail.com>
2023-04-18 20:12:35 -0400

    Changed favicon and header

--- components/Header.js
+++ components/Header.js
@@ -8,8 +8,7 @@ function Header() {
     return (
         <>
             <header className={`flex justify-center items-center h-[96px] px-10 bg-blue-300 box-border relative`} id='navbar'>
-                {/* <span className="material-symbols-outlined invisible">account_circle</span> */}
-                <Image src="/logo-light.png" height={64} width={64} alt="Logo" className='h-auto' />
+                <Image src="/logo-light.svg" height={256} width={256} alt="Logo" className='h-auto' />
                 <div className={`absolute right-0 h-full w-16 mr-10 flex items-center justify-center cursor-pointer`}
                     onMouseOver={() => setAccPaneOpen(true)}
                     onMouseLeave={() => setAccPaneOpen(false)}>
--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=optional" />
-        <link rel='icon' type='image/png' sizes='32x32' href="/favicon-32x32.png" />
-        <link rel='icon' type='image/png' sizes='16x16' href="/favicon-16x16.png" />
+        <link rel="icon" href='/favicon.png' type="image/png" />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
       <body>
         <Main />
