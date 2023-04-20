b7691734726c26af73ede57de86a8f06c69a2b4d
JTris1 <johnathontrisler@gmail.com>
2023-04-19 20:20:24 -0400

    Improved styling and added to landing page

--- components/ZipCodeSearch.js
+++ components/ZipCodeSearch.js
@@ -10,7 +10,9 @@ function ZipCodeSearch({ setSearchInput }) {
     }
 
     return (
-        <input className={`border-solid border-4 border-blue-500 rounded-xl w-full -ml-4 h-14 text-2xl py-2 px-4 focus:outline-none`} id='zipcode-home-search'
+        // <input className={`border-solid border-4 border-blue-500 rounded-xl w-full -ml-4 h-14 text-2xl py-2 px-4 focus:outline-none`} 
+        <input className={`border-solid border-2 border-blue-300 bg-white rounded-xl h-14 w-full text-2xl -ml-4 py-2 px-4 transition-shadow focus:outline-none focus:[box-shadow:0_0_1px_3px_rgba(3,64,120,0.5)]`}
+            id='zipcode-home-search'
             type="text" name="zipcode"
             placeholder={currentCode}
             onInput={(e) => setSearchInput(e.target.value)}
