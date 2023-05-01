f837e5cb2d1363f0159b68ba1fdf685c68e17c41
JTris1 <johnathontrisler@gmail.com>
2023-05-01 16:50:19 -0400

    Changed a few more icons to local versions

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,5 +1,9 @@
 import Image from 'next/image'
 import React from 'react'
+import Bed from './icons/Bed';
+import Bathtub from './icons/Bathtub';
+import SqFt from './icons/SqFt';
+import PinDrop from './icons/PinDrop';
 
 function ListItem({ price, latlng, bedrooms, bathrooms, floor_size_sq_ft, description, locality, panTo }) {
     let USD = new Intl.NumberFormat('en-US', {
@@ -11,7 +15,7 @@ function ListItem({ price, latlng, bedrooms, bathrooms, floor_size_sq_ft, descri
     return (
         <div className='h-40 bg-blue-500 text-white my-5 rounded-xl flex'>
             <div className='relative w-1/3'>
-                <Image src="example-house.jpg" alt='Home Image' fill={true} sizes="450px" className='object-cover rounded-s-xl z-50' />
+                <Image src="/example-house.jpg" alt='Home Image' fill={true} sizes="450px" className='object-cover rounded-s-xl z-50' />
             </div>
             <div className='w-fit flex justify-evenly px-2 py-3 relative'>
                 <Column>
@@ -22,15 +26,15 @@ function ListItem({ price, latlng, bedrooms, bathrooms, floor_size_sq_ft, descri
                     </span>
                     <div className='flex justify-between w-fit md:flex-col md:m-5'>
                         <span className='inline-flex items-center' >
-                            <span className="material-symbols-outlined text-2xl mx-2">bed</span>
+                            <Bed className={'fill-white w-7 mx-2'} />
                             <p className='text-xl font-bold'>{bedrooms}</p>
                         </span>
                         <span className='inline-flex items-center'>
-                            <span className="material-symbols-outlined text-2xl mx-2">bathtub</span>
+                            <Bathtub className={'fill-white w-7 mx-2'} />
                             <p className='text-xl font-bold'>{bathrooms}</p>
                         </span>
                         <span className='inline-flex items-center'>
-                            <span className="material-symbols-outlined text-2xl mx-2">square_foot</span>
+                            <SqFt className={'fill-white w-7 mx-2'} />
                             <p className='text-xl font-bold'>{floor_size_sq_ft} <span className='text-sm'>ft<sup>2</sup></span></p>
                         </span>
                     </div>
@@ -42,8 +46,9 @@ function ListItem({ price, latlng, bedrooms, bathrooms, floor_size_sq_ft, descri
                     </div>
 
                 </Column>
-                <span className="material-symbols-outlined text-2xl absolute bg-white text-blue-500 w-8 h-8 rounded-xl text-center right-4 cursor-pointer"
-                    onClick={() => { panTo(latlng.lat, latlng.lng) }}>pin_drop</span>
+                <span className='bg-white w-8 h-8 rounded-xl absolute right-4 cursor-pointer flex justify-center items-center' onClick={() => { panTo(latlng.lat, latlng.lng) }}>
+                    <PinDrop className={'fill-blue-500 w-6'} />
+                </span>
             </div>
 
         </div>
